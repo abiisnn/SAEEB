21bb44afb2af1e12b59d3a4878f690f847648678
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2018-05-26 16:57:41 -0500

    Add files via upload
    Archivo para conectar a la BD

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+	function conectar(){
+		$user="root";
+		$pass="root";
+		$server="localhost";
+		$db="saeeb";
+		$con=mysqli_connect($server, $user, $pass,$db);
+
+		if (mysqli_connect_errno()){
+  			echo "Error al conectar a la BD: " . mysqli_connect_error();
+  		}
+		return $con;
+	}
+?>
