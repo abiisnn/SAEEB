e9aa1e8e06ef9325dbf4e3b3d142457de09d8d5f
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2018-05-27 12:16:54 -0500

    Mensajes enviados por usuario

---
+++ SAEEB/enviados.php
@@ -0,0 +1,46 @@
+<html>
+<title>
+	Bandeja de Entrada - SAEEB
+</title>
+<body>
+
+	
+	<br>
+<?php
+	include("conexion.php");
+	include("obtenerUsuario.php");
+	$con=conectar();
+	if (!$con)
+  			die ("Error al conectar a la BD: ". mysqli_connect_error());
+  	else{
+  		$ingresa=true;
+		$idusuario=280001;
+
+		echo "Bandeja de entrada. <br>Usuario: $idusuario - ".usuario($idusuario, $con);
+		echo "<br><br><br><br>"; 
+
+		echo "<a href='bandeja.php?idRemitente=$idusuario'><button>Recibidos</button></a>";
+
+		echo "<a href='enviados.php?idRemitente=$idusuario'><button>Enviados</button></a>";
+
+		echo "<a href='nuevoMensaje.php?idRemitente=$idusuario'><button>Redactar</button></a>";
+		echo "<br><br>";
+
+		$mensaje=mysqli_query($con, "SELECT destinatario, idmensaje, asunto, horamensaje FROM mensaje WHERE idusuario=$idusuario ORDER BY 4 DESC"); 
+
+		if (mysqli_num_rows($mensaje)) { 
+			while ($rowMensaje = mysqli_fetch_array($mensaje)) { 
+
+				echo "<a href='mensaje.php?idMensaje=$rowMensaje[1]'>$rowMensaje[0] ".usuario($rowMensaje[0], $con)." $rowMensaje[2] $rowMensaje[3]</a>"; 
+				echo "<br><hr>"; 
+		}
+
+		}
+		else{ 
+			echo "Todavia no ha enviado mensajes.<br><br><br><br>";
+		}
+	}
+
+?>
+</body>
+</html>
