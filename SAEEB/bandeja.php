3c6e565387b9f30e07e6cc932a1a9c6cfd4e82b7
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2018-05-27 12:23:08 -0500

    Update bandeja.php

--- SAEEB/bandeja.php
+++ SAEEB/bandeja.php
@@ -13,13 +13,17 @@
 	if (!$con)
   			die ("Error al conectar a la BD: ". mysqli_connect_error());
   	else{
-
+  		$ingresa=true;
 		$idusuario=280001;
 
-		echo "Bandeja de entrada. Usuario: ".usuario($idusuario, $con);
+		echo "Bandeja de entrada. <br>Usuario: $idusuario - ".usuario($idusuario, $con);
 		echo "<br><br><br><br>"; 
 
-		echo "<a href='nuevoMensaje.php?idRemitente=$idusuario'><button>Nuevo Mensaje</button></a>";
+		echo "<a href='bandeja.php?idRemitente=$idusuario'><button>Recibidos</button></a>";
+
+		echo "<a href='enviados.php?idRemitente=$idusuario'><button>Enviados</button></a>";
+
+		echo "<a href='nuevoMensaje.php?idRemitente=$idusuario'><button>Redactar</button></a>";
 		echo "<br><br>";
 
 		$mensaje=mysqli_query($con, "SELECT idusuario, idmensaje, asunto, horamensaje FROM mensaje WHERE destinatario=$idusuario ORDER BY 4 DESC"); 
@@ -27,7 +31,7 @@
 		if (mysqli_num_rows($mensaje)) { 
 			while ($rowMensaje = mysqli_fetch_array($mensaje)) { 
 
-				echo "<a href='mensaje.php?idMensaje=$rowMensaje[1]'>$rowMensaje[0] ".usuario($rowMensaje[0], $con)." $rowMensaje[1] $rowMensaje[2] $rowMensaje[3]</a>"; 
+				echo "<a href='mensaje.php?idMensaje=$rowMensaje[1]'>$rowMensaje[0] ".usuario($rowMensaje[0], $con)." $rowMensaje[2] $rowMensaje[3]</a>"; 
 				echo "<br><hr>"; 
 		}
 
