911984926acebea4ed734482479b82ee715f738b
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2018-05-27 20:10:37 -0500

    Cerrar Sesión
    Pagina principal del Alumno

--- SAEEB/Principal_Alumno.php
+++ SAEEB/Principal_Alumno.php
@@ -39,21 +39,11 @@ if($now > $_SESSION['expire'])
 				<header id="header" class="alt">
 					<nav id="nav">
 						<ul>
-							<li><a href="Principal_Alumno.html">Inicio</a></li>
+							<li><a href="Principal_Alumno.php">Inicio</a></li>
 							<li>
 								<a href="#" class="icon fa-angle-down">Opciones</a>
 								<ul>
-									<li><a href="generic.html">Contacto</a></li>
-									<li><a href="contact.html">Cerrar Sesión</a></li>
-									<li>
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a href="#">Option One</a></li>
-											<li><a href="#">Option Two</a></li>
-											<li><a href="#">Option Three</a></li>
-											<li><a href="#">Option Four</a></li>
-										</ul>
-									</li>
+									<li><a href="CerrarSesion.php">Cerrar Sesión</a></li>
 								</ul>
 							</li>
 						</ul>
