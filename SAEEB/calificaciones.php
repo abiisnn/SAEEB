e7d456d5ffe84af0aaf22634bb12084b1a0b802c
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2018-05-28 19:50:01 -0500

    Calificaciones
    Voy a empezar la parte de calificaciones.

---
+++ SAEEB/calificaciones.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
+{
+
+}
+else 
+{
+	header("Location: index.html");
+	exit;
+}
+$now = time();
+if($now > $_SESSION['expire'])
+{
+	session_destroy();
+	header("Location: index.html");	
+}
+?>
