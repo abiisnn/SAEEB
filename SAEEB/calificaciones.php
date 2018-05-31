c5fbbfa25e6b9107785c441e49101c9853571574
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2018-05-31 09:54:35 -0500

    Editando Calificaciones
    Para Alumno

--- SAEEB/calificaciones.php
+++ SAEEB/calificaciones.php
@@ -16,3 +16,62 @@ if($now > $_SESSION['expire'])
 	header("Location: index.html");	
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calificaciones</title>
+</head>
+<body>
+
+<?php
+	include("conexion.php");
+	include("obtenerUsuario.php");
+	$con = conectar();
+	if($con)
+	{
+		$usuario = $_SESSION['username'];
+		$Tipo = tipoUsuario($usuario, $con);
+
+		if($Tipo == "ALUMNO")
+		{
+
+			$sql = "SELECT m.idMateria,m.Nombre,x.Calificacion from 	Materia m,am x where m.idmateria=x.idmateria and idAlumno='$usuario";
+			$result = mysqli_query($con, $sql);
+			$Cal=$result->fetch_array();
+			echo "<p>".$Cal[0]."</p>";
+
+
+			$res2 = mysqli_query($con,"SELECT m.idMateria,m.Nombre,x.Calificacion from 	Materia m,am x where m.idmateria=x.idmateria and idAlumno='$idAlumno'");
+  		if (mysqli_num_rows($res2)){
+			while ($row = mysqli_fetch_array($res2)) 
+			{
+					echo"
+						 <tr>
+    						<td>$row[0]</td>
+   							<td>$row[1]</td> 
+    						<td>$row[2]</td>
+  						</tr>
+					";
+			}
+		}
+		}
+		else
+		{
+			if($Tipo == "PROFESOR")
+			{
+
+			}
+			else
+			{
+
+			}
+		}
+	}
+	 
+?>
+
+
+
+</body>
+</html>
